4cd145cd60f4c4280be7ddb72c052d497b6e9fb7
rafalsocha321 <126696041+rafalsocha321@users.noreply.github.com>
2023-03-15 15:07:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    //System.out.println("Hello world!");
+    String tekst="witam witam to testowy program";
+    System.out.println(tekst.replace(' ', '_'));
+  }
+}
